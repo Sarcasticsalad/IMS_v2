1a3900b94c946a2f87e3f0864f27d403a2811313
Sarcasticsalad <samrajyabasnyat64@gmail.com>
2025-07-26 15:32:21 +0545

    Added the paths for signup and login

--- ims_project/authentication/urls.py
+++ ims_project/authentication/urls.py
@@ -5,7 +5,9 @@ app_name = 'authentication'  # Define the app namespace
 
 urlpatterns = [
     path('', views.login_view, name='login'),  # Login view
+    path('signup/', views.signup_view, name='signup'), # Signup View
     path('set-password/', views.set_password, name='set_password'),  # Set password view
     path('logout/', views.logout_view, name='logout'),  # Logout view
     path('redirect/', views.post_login_redirect, name='post_login_redirect'),  # Post-login redirect view
+
 ]
